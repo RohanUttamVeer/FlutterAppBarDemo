ead37458d60182fbcf85fb3af37639d1b158bffb
RohanUttamVeer <rohanveer04@gmail.com>
2021-02-23 16:39:33 +0530

    action theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,34 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         //1
-        title: Text("AppBar Demo"), //App bar heading title
+        title: Text("AppBar Demo"),
+        //App bar heading title
         //2
-        centerTitle: true,//heading centered
+        centerTitle: true,
+        //heading centered
         //3
         backgroundColor: Color(0xFF85C675),
         //4
-        elevation: 4, // shadow under appbar
+        elevation: 4,
+        // shadow under appbar
         // leading: IconButton( // icons o the right of appbar
         //   icon: Icon(Icons.settings),
         //5
-        actions: <Widget>[
-          IconButton( //icons on the left of appbar
+        actionsIconTheme: IconThemeData(color: Colors.red), //theme for action buttons
+        //6
+        actions: <Widget>[ //action buttons on appbar on the left
+          IconButton(
+            //icons on the left of appbar
             icon: Icon(Icons.add_circle_outline),
             onPressed: () => debugPrint("Rohan"),
           ),
           IconButton(
             icon: Icon(Icons.wb_cloudy),
             onPressed: () => debugPrint("Veer"),
-          )
+          ),
         ],
       ),
-      //6
+      //7
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
