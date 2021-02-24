e3d8ae3f2f005ab05c9c8264d99c8ca2cd4b07f1
RohanUttamVeer <rohanveer04@gmail.com>
2021-02-24 17:52:02 +0530

    new Properties added(Brightness, Bottom[PreferredSize])

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, // debug banner removed
     title: "AppBar",
     home: Scaffold(
       appBar: AppBar(
         //1
         title: Text("AppBar Demo"),
-        automaticallyImplyLeading: false, // to disable/enable the leading widget
+        automaticallyImplyLeading: true,
+        // to disable/enable the leading widget
         //App bar heading title
         //2
         centerTitle: true,
         //heading centered
         //3
         backgroundColor: Color(0xFF85C675),
+        brightness: Brightness.light,
+        //brightness of appBar to Light/Dark
         //4
         elevation: 0,
         // shadow under appbar
+        bottom: PreferredSize(
+          //size added in the bottom of the app bar
+          preferredSize: Size.fromHeight(40),
+          child: Text("This is a bottom Text"),
+        ),
         // leading: IconButton( // icons on the left of appbar
         //     icon: Icon(Icons.settings)),
         //5
         actionsIconTheme: IconThemeData(color: Colors.red),
         //theme for action buttons
         //6
-        actions: <Widget>[  //action buttons on appbar on the right
+        actions: <Widget>[
+          //action buttons on appbar on the right
           IconButton(
             //icons on the left of appbar
             icon: Icon(Icons.add_circle_outline),
