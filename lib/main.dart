0d2a3a154be4c009db5f7348bf17eeca460a0b75
RohanUttamVeer <rohanveer04@gmail.com>
2021-02-24 19:12:02 +0530

    Shape, iconTheme, shadowColor added to the AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,35 +8,35 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         //1
-        title: Text("AppBar Demo"),
-        automaticallyImplyLeading: true,
-        // to disable/enable the leading widget
-        //App bar heading title
+        title: Text("AppBar Demo"),//App bar heading title
+        automaticallyImplyLeading: true, // to disable/enable the leading widget
         //2
         centerTitle: true,
         //heading centered
         //3
         backgroundColor: Color(0xFF85C675),
-        brightness: Brightness.light,
-        //brightness of appBar to Light/Dark
+        brightness: Brightness.light, //brightness of appBar to Light/Dark
         //4
-        elevation: 0,
-        // shadow under appbar
-        bottom: PreferredSize(
-          //size added in the bottom of the app bar
+        elevation: 10, // shadow under appbar
+        shadowColor: Colors.red, // elevation shadow color changed
+        shape: RoundedRectangleBorder( //shapes your appbar
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40),
+          bottomRight: Radius.circular(40),
+        )),
+        iconTheme: IconThemeData(opacity: 0.6), // icons faded
+        bottom: PreferredSize(  //size added in the bottom of the app bar
           preferredSize: Size.fromHeight(40),
           child: Text("This is a bottom Text"),
         ),
         // leading: IconButton( // icons on the left of appbar
         //     icon: Icon(Icons.settings)),
         //5
-        actionsIconTheme: IconThemeData(color: Colors.red),
-        //theme for action buttons
+        actionsIconTheme: IconThemeData(color: Colors.red), //theme for action buttons
         //6
-        actions: <Widget>[
-          //action buttons on appbar on the right
+        actions: <Widget>[ //action buttons on appbar on the right
           IconButton(
-            //icons on the left of appbar
+            iconSize: 40, //icons on the left of appbar
             icon: Icon(Icons.add_circle_outline),
             onPressed: () => debugPrint("Rohan"),
           ),
